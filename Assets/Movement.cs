5b87b973dfe11792a70e7f211e7c51281992f773
david.pohlman <david.pohlman@elev.ga.lbs.se>
2018-10-16 10:50:54 +0200

    Fixade en lävla svår labyrint.

---
+++ Assets/Movement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//AddForce
+//AddRelativeForce
+//AddTorque
+//MovePosition
+//MoveRotation
+//Velocity
+//angularVelocity
+public class Movement : MonoBehaviour
+{
+    public Rigidbody2D rbody;
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+    //Lägg allt som har med fysik att göra här.
+    void FixedUpdate()
+    {
+        
+
+        if (Input.GetKey(KeyCode.S))
+        {
+
+            rbody.AddForce(new Vector2(0f, -10f));
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+
+            rbody.AddForce(new Vector2(0f, 10f));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+
+            rbody.AddForce(new Vector2(10f, 0f));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+
+            rbody.AddForce(new Vector2(-10f, 0f));
+        }
+    }
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
