9bf883432d66dcf20cfa960e53fdd9db66d059a0
david.pohlman <david.pohlman@elev.ga.lbs.se>
2018-10-19 15:32:58 +0200

    Animation

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public Animator anim;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("VadNiVill", true);
+        }
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            anim.SetBool("VadNiVill", false);
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            anim.SetTrigger("AAA");
+        }
+    }
+}
--- Assets/Rotation.cs
+++ Assets/Rotation.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotation : MonoBehaviour {
+public class Rotaion : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
